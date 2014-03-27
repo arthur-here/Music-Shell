e811b609f1a0d91b9ffd327173dc34236f55248f
burntheroad <gibslp69@gmail.com>
2014-03-27 18:40:19 +0200

    added getAlbums

---
+++ VK-Player/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VK_Player
+{
+    class Album
+    {
+        public string album_id;
+        public string owner_id;
+        public string title;
+    }
+}
